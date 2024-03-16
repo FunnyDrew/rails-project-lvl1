53685d429e935ace69580614a2d6609bf678984f
FunnyDrew <rybin.ag@gmail.com>
2024-03-16 22:31:24 +0300

    fix tag.rb

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -4,26 +4,20 @@ module HexletCode
   module Tag
     SINGLE_TAGS = %w[br img input].freeze
 
-    def self.build(tag, arg = {}, &body)
-      init = ["<#{tag}"]
-      acc = init + arg_pack(arg)
-      if SINGLE_TAGS.include? tag
-        html = "#{acc.join(' ')}>"
-      else
-        content = body.call if block_given?
-        html = acc.join(' ') + ">#{content}</#{tag}>"
-      end
-      html
+    def self.build(tag, attributes = {}, &body)
+      attr_list = build_attributes(attributes)
+      html = "<#{tag}#{attr_list.join}>"
+      return html if SINGLE_TAGS.include? tag
+
+      content = body.call if block_given?
+
+      "#{html}#{content}</#{tag}>"
     end
 
-    def arg_pack(arg)
-      atributes = []
-      arg.each do |key, value|
-        atributes << ("#{key}=\"#{value}\"")
+    def self.build_attributes(attr)
+      attr.map do |key, value|
+        " #{key}=\"#{value}\""
       end
-      atributes
     end
-
-    module_function :arg_pack
   end
 end
